22a031d011dd2d4bce7de2086f32d3983cd5687a
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-10-25 10:05:18 +0800

    Update tests.py

--- healthcaretether/main/tests.py
+++ healthcaretether/main/tests.py
@@ -1,3 +1,5 @@
+from tokenize import Name
+from webbrowser import get
 from django.test import TestCase
 from main.models import *
 
@@ -6,3 +8,9 @@ class hospitalTestCase(TestCase):
     def setUp(self):
         Hospital.objects.create(name="Tan Tock Seng Hospital")
         Hospital.objects.create(name="National University Hospital")
+    
+    def test_hospital_name(self):
+        TTS = Hospital.objects.get(name="Tan Tock Seng Hospital")
+        NUH = Hospital.objects.get(name="National University Hospital")
+        self.assertEqual(TTS = "Tan Tock Seng Hospital")
+        self.assertEqual(NUH = "National University Hospital")        
