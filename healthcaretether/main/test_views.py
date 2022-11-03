0b17df8e92f0d9aec82061cdc31bcfa8cccfeed4
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-11-03 17:28:44 +0800

    Update test_views.py

--- healthcaretether/main/test_views.py
+++ healthcaretether/main/test_views.py
@@ -10,15 +10,14 @@ class test_login(TestCase):
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_login(self):
-
-        response = self.client.login(self, username='test',password='test')   
+        response = self.client.post('/auth/login', {'username':'test', 'password': 'test'})
         self.assertEqual(response.status_code, 200)
     
     def test_login_success(self):
         user = CustomUser.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
-        response = self.client.login(self, username='test',password='test')        
+        response = self.client.login(username='testuser', password='12345')
         self.assertEqual(response, True)
         test = self.client.post('/auth/login', {'username':'testuser', 'password': '12345'})
         self.assertEqual(test.status_code, 302)
