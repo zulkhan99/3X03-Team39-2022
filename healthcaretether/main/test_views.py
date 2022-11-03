c7a6308d5f4615b1404feca8098aa060a4e2653c
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-11-03 13:40:34 +0800

    hotfix: update login test with for axes

--- healthcaretether/main/test_views.py
+++ healthcaretether/main/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from main.models import *
 from main.views import *
+from django.http import HttpRequest
 
 
 class test_login(TestCase):
@@ -10,14 +11,16 @@ class test_login(TestCase):
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_login(self):
-        response = self.client.post('/auth/login', {'username':'test', 'password': 'test'})
+        request = HttpRequest()
+        response = authenticate(request,username='test',password='test')
         self.assertEqual(response.status_code, 200)
     
     def test_login_success(self):
         user = CustomUser.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
-        response = self.client.login(username='testuser', password='12345')
+        request = HttpRequest()
+        response = authenticate(request,username='testuser', password='12345')
         self.assertEqual(response, True)
         test = self.client.post('/auth/login', {'username':'testuser', 'password': '12345'})
         self.assertEqual(test.status_code, 302)
