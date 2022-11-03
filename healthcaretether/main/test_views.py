0b7fdfe598adb6790212bcc512d273ce7ebdc698
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-11-03 17:23:49 +0800

    hotfix: wrong log class

--- healthcaretether/main/test_views.py
+++ healthcaretether/main/test_views.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from main.models import *
 from main.views import *
-from django.http import HttpRequest
 
 
 class test_login(TestCase):
@@ -11,16 +10,15 @@ class test_login(TestCase):
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_login(self):
-        request = HttpRequest()
-        response = authenticate(request,username='test',password='test')
+
+        response = self.client.login(self, username='test',password='test')   
         self.assertEqual(response.status_code, 200)
     
     def test_login_success(self):
         user = CustomUser.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
-        request = HttpRequest()
-        response = authenticate(request,username='testuser', password='12345')
+        response = self.client.login(self, username='test',password='test')        
         self.assertEqual(response, True)
         test = self.client.post('/auth/login', {'username':'testuser', 'password': '12345'})
         self.assertEqual(test.status_code, 302)
