554c6f393cd7bfe679a559772ce917e2c35a5148
jinng1 <72145074+jinng1@users.noreply.github.com>
2022-11-06 11:49:44 +0800

    Update test_views.py

--- healthcaretether/main/test_views.py
+++ healthcaretether/main/test_views.py
@@ -1,25 +1,25 @@
-from django.test import TestCase, override_settings
-from main.models import *
-from main.views import *
+# from django.test import TestCase, override_settings
+# from main.models import *
+# from main.views import *
 
-@override_settings(AXES_ENABLED=False)
-class test_login(TestCase):
-    def test_can_access_login_page(self):
-        response = self.client.get("/auth/login")
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'registration/login.html')
+# @override_settings(AXES_ENABLED=False)
+# class test_login(TestCase):
+#     def test_can_access_login_page(self):
+#         response = self.client.get("/auth/login")
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_login(self):
-        response = self.client.post('/auth/login', {'username':'test', 'password': 'test'})
-        self.assertEqual(response.status_code, 200)
+#     def test_login(self):
+#         response = self.client.post('/auth/login', {'username':'test', 'password': 'test'})
+#         self.assertEqual(response.status_code, 200)
     
-    def test_login_success(self):
-        user = CustomUser.objects.create(username='testuser')
-        user.set_password('12345')
-        user.save()
-        response = self.client.login(username='testuser', password='12345')
-        self.assertEqual(response, True)
-        test = self.client.post('/auth/login', {'username':'testuser', 'password': '12345'})
-        self.assertEqual(test.status_code, 302)
+#     def test_login_success(self):
+#         user = CustomUser.objects.create(username='testuser')
+#         user.set_password('12345')
+#         user.save()
+#         response = self.client.login(username='testuser', password='12345')
+#         self.assertEqual(response, True)
+#         test = self.client.post('/auth/login', {'username':'testuser', 'password': '12345'})
+#         self.assertEqual(test.status_code, 302)
 
-#testing jenkins
+# #testing jenkins
